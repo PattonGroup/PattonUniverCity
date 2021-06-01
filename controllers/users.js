f032a6db0bb6c0ba5f07f9a1459044c7151f76ea
Abderrahmenla <abdo.vic@hotmail.com>
2021-06-02 00:12:43 +0100

    fix: fixing className,Link errors

--- controllers/users.js
+++ controllers/users.js
@@ -1,112 +1,105 @@
-const User = require("../models/userModel");
-const bcrypt = require("bcryptjs");
-const mongoose = require("mongoose");
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 
-
 const login = async (req, res, next) => {
-	try {
-		const { email, password } = req.body;
-		const user = await User.findOne({ email })
-        if (!user) return res.status(400).json({ msh: "User does not exist." })
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msh: 'User does not exist.' });
 
-        const isMatch = await bcrypt.compare(password, user.password)
-        if (!isMatch) return res.status(400).json({ msh: "Invalid password" })
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msh: 'Invalid password' });
 
-		const accesstoken = createAccessToken({ id: user._id })
-        const refreshtoken = createRefreshToken({ id: user._id })
+    const accesstoken = createAccessToken({ id: user._id });
+    const refreshtoken = createRefreshToken({ id: user._id });
 
-		res.json({ accesstoken, msg: "Logged in"})
-
-	}
-	catch {
-        return res.status(500).json({ msg: err.message })
-	}
+    res.json({ accesstoken, msg: 'Logged in' });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
 };
-
 
 const logout = (req, res) => {
-	req.logout();
-	req.session.destroy();
-	res.send({ msg: "Successfully logged out" });
+  req.logout();
+  req.session.destroy();
+  res.send({ msg: 'Successfully logged out' });
 };
 
-
 const register = async (req, res) => {
-	try {
-		const { email, username, password, fname, lname, fullname } = req.body;
-		await User.findOne({ username }, async (err, doc) => {
-			if (err) throw err;
-			if (doc) res.send({ msg: "User already exists" });
-			if (!doc) {
-				const hashedPassword = await bcrypt.hash(password, 10);
-				if (username === "admin") {
-					const newUser = new User({
-						_id: new mongoose.Types.ObjectId(),
-						username: username,
-						password: hashedPassword,
-						email: email,
-						fname: fname,
-						lname: lname,
-						fullname: fullname,
-						cart: { items: [], total_price: 0 },
-						isAdmin: true,
-					});
-					await newUser.save();
-					res.send({ msg: "User created" });
-				} else {
-					const newUser = new User({
-						_id: new mongoose.Types.ObjectId(),
-						username: username,
-						password: hashedPassword,
-						email: email,
-						fname: fname,
-						lname: lname,
-						fullname: fullname,
-						cart: { items: [], total_price: 0 },
-						isAdmin: false,
-					});
-					await newUser.save();
-					res.send({ msg: "User created" });
-				}
-			}
-		});
-	} catch (err) {
-		console.log(err)
-        return res.status(400).json({ msg: err.message })
-    }
-	
-
+  try {
+    const { email, username, password, fname, lname, fullname } = req.body;
+    await User.findOne({ username }, async (err, doc) => {
+      if (err) throw err;
+      if (doc) res.send({ msg: 'User already exists' });
+      if (!doc) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        if (username === 'admin') {
+          const newUser = new User({
+            _id: new mongoose.Types.ObjectId(),
+            username: username,
+            password: hashedPassword,
+            email: email,
+            fname: fname,
+            lname: lname,
+            fullname: fullname,
+            cart: { items: [], total_price: 0 },
+            isAdmin: true,
+          });
+          await newUser.save();
+          res.send({ msg: 'User created' });
+        } else {
+          const newUser = new User({
+            _id: new mongoose.Types.ObjectId(),
+            username: username,
+            password: hashedPassword,
+            email: email,
+            fname: fname,
+            lname: lname,
+            fullname: fullname,
+            cart: { items: [], total_price: 0 },
+            isAdmin: false,
+          });
+          await newUser.save();
+          res.send({ msg: 'User created' });
+        }
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ msg: err.message });
+  }
 };
 
 const checkUser = async (req, res) => {
-	const userId = req.session.user;
-	if (userId) {
-		User.findById({ _id: userId })
-			.populate("transactions")
-			.exec((err, user) => {
-				if (err) res.send({ msg: "Error in populating transactions" });
-				else if (user) {
-					res.send({ msg: "You are logged in", user: user });
-				}
-			});
-	} else {
-		res.send({ msg: "No user logged in" });
-	}
+  const userId = req.session.user;
+  if (userId) {
+    User.findById({ _id: userId })
+      .populate('transactions')
+      .exec((err, user) => {
+        if (err) res.send({ msg: 'Error in populating transactions' });
+        else if (user) {
+          res.send({ msg: 'You are logged in', user: user });
+        }
+      });
+  } else {
+    res.send({ msg: 'No user logged in' });
+  }
 };
 
 const createAccessToken = (user) => {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' })
-}
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' });
+};
 const createRefreshToken = (user) => {
-    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
-}
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
+};
 
 const userCtrl = {
-    login,
-	checkUser,
-	logout,
-	register
-}
+  login,
+  checkUser,
+  logout,
+  register,
+};
 
-module.exports = userCtrl
+module.exports = userCtrl;
--- src/components/grid/stories.jsx
+++ src/components/grid/stories.jsx
@@ -3,54 +3,78 @@ import './stories.css';
 import Spolight from '../../assets/images/spotlight.jpeg';
 
 const Stories = () => {
-    return (
-        <>
-            <div className="container story-feature">
-                <h2>Patton U Today</h2>
-                <h3 className="story-subheading">The latest stories from Patton U</h3>
-                <main class="story-grid">
-                    <article className="article-one">
-                        <img src={Spolight} alt="Sample spotlight"/>
-                        <h5 className="article-one-title"><span>UniverCity Affairs</span></h5><br/>
-                        <h6 className="article-one-subtitle"><span>Patton to continue 11 varsity sports</span></h6><br/>
-                    </article>
-                    <article>
-                        <img src={Spolight} alt="Sample spotlight"/>
-                        <div class="story-text">
-                        <h3>Completely Synergize</h3>
-                        <p>Dramatically engage seamlessly visualize quality intellectual capital without superior collaboration and idea-sharing.</p>
-                        </div>
-                    </article>
-                    <article>
-                        <img src={Spolight} alt="Sample spotlight"/>
-                        <div class="story-text">
-                        <h3>Dynamically Procrastinate</h3>
-                        <p>Completely synergize resource taxing relationships via premier niche markets.</p>
-                        </div>
-                    </article>
-                    <article>
-                        <img src={Spolight} alt="Sample spotlight"/>
-                        <div class="story-text">
-                        <h3>Best in class</h3>
-                        <p>Imagine jumping into that boat, and just letting it sail wherever the wind takes you...</p>
-                        </div>
-                    </article>
-                    <article>
-                        <img src={Spolight} alt="Sample spotlight"/>
-                        <div class="story-text">
-                        <h3>Dynamically innovate supply chains</h3>
-                        <p>Holisticly predominate extensible testing procedures for reliable supply chains.</p>
-                        </div>
-                    </article>
-                    <article className="article-one">
-                        <img src={Spolight} alt="Sample spotlight"/>
-                        <h5 className="article-one-title"><span>UniverCity Affairs</span></h5><br/>
-                        <h6 className="article-one-subtitle"><span>Patton to continue 11 varsity sports</span></h6><br/>
-                    </article>
-                </main>
+  return (
+    <>
+      <div className="container story-feature">
+        <h2>Patton U Today</h2>
+        <h3 className="story-subheading">The latest stories from Patton U</h3>
+        <main className="story-grid">
+          <article className="article-one">
+            <img src={Spolight} alt="Sample spotlight" />
+            <h5 className="article-one-title">
+              <span>UniverCity Affairs</span>
+            </h5>
+            <br />
+            <h6 className="article-one-subtitle">
+              <span>Patton to continue 11 varsity sports</span>
+            </h6>
+            <br />
+          </article>
+          <article>
+            <img src={Spolight} alt="Sample spotlight" />
+            <div className="story-text">
+              <h3>Completely Synergize</h3>
+              <p>
+                Dramatically engage seamlessly visualize quality intellectual
+                capital without superior collaboration and idea-sharing.
+              </p>
             </div>
-        </>
-    )
-}
+          </article>
+          <article>
+            <img src={Spolight} alt="Sample spotlight" />
+            <div className="story-text">
+              <h3>Dynamically Procrastinate</h3>
+              <p>
+                Completely synergize resource taxing relationships via premier
+                niche markets.
+              </p>
+            </div>
+          </article>
+          <article>
+            <img src={Spolight} alt="Sample spotlight" />
+            <div className="story-text">
+              <h3>Best in class</h3>
+              <p>
+                Imagine jumping into that boat, and just letting it sail
+                wherever the wind takes you...
+              </p>
+            </div>
+          </article>
+          <article>
+            <img src={Spolight} alt="Sample spotlight" />
+            <div className="story-text">
+              <h3>Dynamically innovate supply chains</h3>
+              <p>
+                Holisticly predominate extensible testing procedures for
+                reliable supply chains.
+              </p>
+            </div>
+          </article>
+          <article className="article-one">
+            <img src={Spolight} alt="Sample spotlight" />
+            <h5 className="article-one-title">
+              <span>UniverCity Affairs</span>
+            </h5>
+            <br />
+            <h6 className="article-one-subtitle">
+              <span>Patton to continue 11 varsity sports</span>
+            </h6>
+            <br />
+          </article>
+        </main>
+      </div>
+    </>
+  );
+};
 
 export default Stories;
