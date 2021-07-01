a793022d9c29d3723a16e244e4af3684788c411b
ZacYou <zyou@calstatela.edu>
2021-07-01 16:25:40 -0700

    updated the teamcard component

--- frontend/src/pages/blockchain/main.jsx
+++ frontend/src/pages/blockchain/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import TeamCard from '../../components/teamcard/teamCard';
+import Navbar from '../../components/navbar/content';
+import Footer from '../../components/footer/footer';
 
 const Blockchain = () => {
 
     return(
         <div>
+            <Navbar />
             <TeamCard />
+            <Footer />
         </div>
     )
 }
