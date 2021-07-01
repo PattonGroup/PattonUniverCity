49bb73ed438dfc4179ddd4abb28ca38dc4eabffe
hoseacodes <dominique11h@yahoo.com>
2021-06-30 21:35:09 -0500

    First Commit

--- backend/app.js
+++ backend/app.js
@@ -37,12 +37,11 @@ app.use(
   
 app.use('/api/users', userRoutes);
 
-const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
   );
 }
 
