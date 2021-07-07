b985f72988da87edb9e9975e15274d3c74c922e1
hoseacodes <dominique11h@yahoo.com>
2021-07-07 07:33:57 -0500

    refactor: Adj info inside story component

--- frontend/src/components/grid/stories.jsx
+++ frontend/src/components/grid/stories.jsx
@@ -14,22 +14,24 @@ const Stories = () => {
         <>
           <div className="container story-feature">
             <h2>Patton U Today</h2>
-            <h3 className="story-subheading">The latest stories from Patton U</h3>              
+            <h3 className="story-subheading">The Latest Stories from Patton U</h3>              
             <main class="story-grid">
               <article className="article-one">
-                <img src={Cryptobook} className="img-fluid img-thumbnail" alt="Sample spotlight"/>
+                <img src={'https://i.imgur.com/cI4fMAt.png'} className="img-fluid img-thumbnail" alt="Sample spotlight"/>
                 {/* <h5 className="article-one-title"><span>UniverCity Affairs</span></h5><br/> */}
                 {/* <h6 className="article-one-subtitle"><span>Patton to continue 11 varsity sports</span></h6><br/> */}
               </article>
               <article>
                 <img src={Mascot} className="img-fluid img-thumbnail secondary" alt="Sample spotlight"/>
                 <div class="story-text">
-                  <h3>Yoga and Mindfulness</h3>
-                  <p>Students learn breathing strategies and various body movements to increase flexibility and to relieve stress. This session allows children to center themselves before school and learn coping strategies that will help guide them throughout their day. Meditation begins at 7:30am (CST) every week day morning, beginning June 28 through July 28. Each class is approximately 1/2 hour.</p>
+                  <h3>Summer 2021 Course Catalog is now available! </h3>
+                  <p>Choose from a variety of courses for your student to learn this summer, especially if they are not currently 
+                    active in summer school or some type of summer learning or activity. Sessions last 30 minutes to 1 hour and are 
+                    in preparation for next school year!</p>
                 </div>
               </article>
               <article>
-                <img src={PattonReportCover} className="img-fluid img-thumbnail secondary" alt="Sample spotlight"/>
+                <img src={Cryptobook} className="img-fluid img-thumbnail secondary" alt="Sample spotlight"/>
                 <div class="story-text">
                   <h3>Virtual Book Club</h3>
                   <p>This course is geared towards students in grades 3-8 who want to build reading fluency, vocabulary, and reading comprehension. This is an excellent class for students preparing to take standardized tests! One course is approximately one hour. This course meets twice per week.</p>
@@ -38,15 +40,15 @@ const Stories = () => {
               <article>
                 <img src={SummerGolf} className="img-fluid img-thumbnail secondary" alt="Sample spotlight"/>
                 <div class="story-text">
-                  <h3>Intro to Geometry</h3>
-                  <p>This course will introduce rising 10th graders to new concepts that will be introduced in their Geometry classes next fall. A review of 2-D and 3-D shapes along with formulas to solve various equations will be introduced. Student will get the chance to start the next school year ahead of the game! Each session is $35 and last approximately 45mins-1hour. This class is virtual.</p>
+                  <h3>Golf Lessons for Girls</h3>
+                  <p>Golf Lessons for Girls ages 6-12 in Houston! Visit our Course Catalog to learn more and register!</p>
                 </div>
               </article>
               <article>
                 <img src={PattonReportCover2} className="img-fluid img-thumbnail secondary" alt="Sample spotlight"/>
                 <div class="story-text">
-                  <h3>Summer Enrichment</h3>
-                  <p>This class is 10 sessions long from June 28-July28, meeting twice per week. Students may choose a schedule that best suits their needs (Monday/Wednesday or Tuesday/Thursday). An instructor will meet with students virtually to close academic gaps in Reading, Math, or Science for 30-40 mins per session.</p>
+                  <h3>Patton Report</h3>
+                  <p>Keep up with Patton U news with our monthly Patton Report. Learn about our Spotlight Student and other upcoming events. </p>
                 </div>
               </article>
               <article className="article-one">
