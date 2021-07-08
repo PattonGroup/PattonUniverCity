9b38dede29b2f4999a16bee48ef26a25f1a8c120
amiedawn <amie.d.carroll@gmail.com>
2021-07-08 13:02:15 -0600

    feat: blockchainCTA and blockchainOverlay working and responsive

--- frontend/src/components/overlay/blockchainOverlay.jsx
+++ frontend/src/components/overlay/blockchainOverlay.jsx
@@ -4,12 +4,13 @@ import './blockchainOverlay.css';
 const BlockchainOverlay = () => {
     return (
       <section id="blockchain-slider">
+      <div className="title animated fadeInUp">BLOCKCHAIN</div>
         <div className="container">
             <div className="row">
-                <div className="col-md-6.5">
-                    <div className="block">
-                        <h1 className="animated fadeInUp">Introduction education to the world of Blockchain Technology</h1>
-                    </div>
+              <div className="col-md-6.5">
+                  <div className="block">
+                    <h1 className="animated fadeInUp">Introduction education to the world of Blockchain Technology</h1>
+                  </div>
                 </div>
             </div>
         </div>
