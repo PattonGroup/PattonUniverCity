6911aacf3db4b83691758f38c3cdbce0b3b8f9a5
amiedawn <amie.d.carroll@gmail.com>
2021-07-14 10:06:18 -0600

    feat: blockchainOverlay testing done

--- frontend/src/components/overlay/blockchainOverlay.jsx
+++ frontend/src/components/overlay/blockchainOverlay.jsx
@@ -4,12 +4,12 @@ import './blockchainOverlay.css';
 const BlockchainOverlay = () => {
     return (
       <section id="blockchain-slider">
-      <div className="title animated fadeInUp">BLOCKCHAIN</div>
+      <div className="title animated fadeInUp" data-testid="title-blockchain-overlay">BLOCKCHAIN</div>
         <div className="container">
             <div className="row">
               <div className="col-md-6.5">
                   <div className="block">
-                    <h1 className="animated fadeInUp">Introduction education to the world of Blockchain Technology</h1>
+                    <h1 className="animated fadeInUp" data-testid="header-blockchain-overlay">Introduction education to the world of Blockchain Technology</h1>
                   </div>
                 </div>
             </div>
