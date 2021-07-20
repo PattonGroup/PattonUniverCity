c98d90dafab9b8597714aa92c80f521f8685da60
amiedawn <amie.d.carroll@gmail.com>
2021-07-20 15:46:43 -0600

    feat: quoteHomeFeat testing done

--- frontend/src/components/quotes/quoteHomeFeat.jsx
+++ frontend/src/components/quotes/quoteHomeFeat.jsx
@@ -8,22 +8,22 @@ const QuoteHomeFeat = () => {
         <div className="container">
           <div className="feature-one row">
             <div className="col-md-6">
-              <img src={'https://i.imgur.com/u9ggAMv.jpg'} alt="President Elvonte" />
-              <span className="feature-one-caption"><strong>Photo</strong>: 4th year Patton U student, Bailey speaks at the campaign rally for 
+              <img data-testid="image" src={'https://i.imgur.com/u9ggAMv.jpg'} alt="President Elvonte" />
+              <span data-testid="caption" className="feature-one-caption"><strong>Photo</strong>: 4th year Patton U student, Bailey speaks at the campaign rally for 
                 Beto O’Rouke during the 2018 Presidential Primaries.</span>
             </div>
             <div className="col-md-6">
-              <p className="quote-info">
+              <p data-testid="paragraph1" className="quote-info">
               We cannot predict with certainty the types of jobs that will 
               exist in the future, but we consistently provide students with 
               exposure to 21st century skills with the most authentic, unique, 
-              and innovative learning experiences possible.”
+              and innovative learning experiences possible."
               </p>
-              <p className="quote-info-contact">
+              <p data-testid="paragraph2" className="quote-info-contact">
                 Elvonte Patton, Professor of Education
               </p>
               <div>
-                <button className="quote-btn">→</button>
+                <button data-testid="button" className="quote-btn">→</button>
                 <a href="/" className="quote-link">
                   Learn more about Professor Elvonte
                 </a>
