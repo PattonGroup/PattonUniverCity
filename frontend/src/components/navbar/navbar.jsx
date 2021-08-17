424d13c1ef92249377d298222d2e522e614e5410
jasperisaac27 <jasperisaac27@gmail.com>
2021-08-17 12:16:07 +0800

    Feature/animations on landing page none-responsive

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,101 +1,139 @@
-import React, { useState, useEffect } from 'react';
-import Logo from '../../assets/svg/untitled-design.svg';
-import Hamburger from '../../assets/svg/bar.svg';
-import * as AiIcons from 'react-icons/ai';
-import { SidebarData } from '../../utils/sidebarData';
-import { Link } from 'react-router-dom';
-import './navbar.css';
+import React, { useState, useEffect } from "react";
+import Logo from "../../assets/svg/untitled-design.svg";
+import Hamburger from "../../assets/svg/bar.svg";
+import * as AiIcons from "react-icons/ai";
+import { SidebarData } from "../../utils/sidebarData";
+import { Link } from "react-router-dom";
+import "./navbar.css";
 
 const Navbar = (props) => {
-  const [backgroundOn, setBackgroundOn] = useState(false);
-  const [sidebar, setSidebar] = useState(false);
+	const [backgroundOn, setBackgroundOn] = useState(false);
+	const [sidebar, setSidebar] = useState(false);
 
-  const showSidebar = () => setSidebar(!sidebar);
+	const showSidebar = () => setSidebar(!sidebar);
 
-  const handleScroll = () => {
-    const position = window.pageYOffset;
-    position > 375 ? setBackgroundOn(true) : setBackgroundOn(false);
-  };
+	const handleScroll = () => {
+		const position = window.pageYOffset;
+		position > 375 ? setBackgroundOn(true) : setBackgroundOn(false);
+	};
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true });
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll, { passive: true });
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
-  return (
-    <>
-      <header
-        className={`nav-header sticky-top ${backgroundOn ? 'bg-black' : null}`}
-        data-testid="header-navbar"
-      >
-        <nav className="navbar navbar-default navbar-fixed-top">
-          <a className="py-5" href="/">
-            <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-              <img src={'https://i.imgur.com/RF2Zlgb.png'} alt="Patton U Logo" width="150px" />
-              <h2 className="logo-title">
-                <span className={`${ backgroundOn ? "bg-black" : null}`}>PATTON UNIVERCITY</span> 
-                <br/>
-                <span className={`${ backgroundOn ? "bg-black" : null}`}>Est. 2015</span>
-              </h2>
-            </div>
-          </a>
-          {/* {props.authenticated ? <a href="/dashboard">Dashboard</a> : null} */}
-          {props.authenticated ?
-            <>
-              <a className={`${ backgroundOn ? "bg-black" : null}`} href="/education">Education</a>
-              <a className={`${ backgroundOn ? "bg-black" : null}`} href="/research">Research</a>
-              <a className={`${ backgroundOn ? "bg-black" : null}`} href="/innovation">Innovation</a>
-              <a className={`${ backgroundOn ? "bg-black" : null}`} href="/news">News</a>
-              <a className={`${ backgroundOn ? "bg-black" : null}`} href="/Alumni">Alumni</a>
-              {/* <Link className={`${ backgroundOn ? "bg-black" : null}`} onClick={props.logout}>Logout</Link> */}
-            </>
-            :
-            null
-            // <>
-            //   <a href="/login" style={{ color: "white" }}>Login</a>
-            //   <a href="/signup">Signup</a>
-            // </>
-          }
-        </nav>
-        {/* sidebar */}
-        <div className="mobile-overlay">
-          <div className="main-nav-mobile">
-            <img id="mobile-logo" src={Logo} alt="Logo" />
-            <img
-              id="hamburger"
-              src={Hamburger}
-              alt="Hamburger bar"
-              onClick={showSidebar}
-            />
-          </div> 
-          <div className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-            <ul className="nav-menu-items" onClick={showSidebar}>
-              <li className="navbar-toggle">
-                <img id="mobile-logo" src={Logo} alt="Logo" />
-                <Link to="#" className="menu-bars">
-                  <AiIcons.AiOutlineClose />
-                </Link>
-              </li>
-              {SidebarData.map((link, index) => {
-                return (
-                  <li key={index} className={link.cName}>
-                    <Link to={link.path}>
-                      {link.icon}
-                      <span className="sidebar-title">{link.title}</span>
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        </div>
-      </header>
-      
-    </>
-  );
+	return (
+		<>
+			<header
+				className={`nav-header sticky-top ${backgroundOn ? "bg-black" : null}`}
+				data-testid="header-navbar"
+			>
+				<nav className="navbar navbar-default navbar-fixed-top">
+					<a className="py-5" href="/">
+						<div
+							style={{
+								display: "flex",
+								alignItems: "center",
+								flexDirection: "column",
+							}}
+						>
+							<img
+								src={"https://i.imgur.com/RF2Zlgb.png"}
+								alt="Patton U Logo"
+								width="150px"
+							/>
+							<h2 className="logo-title">
+								<span className={`${backgroundOn ? "bg-black" : null}`}>
+									PATTON UNIVERCITY
+								</span>
+								<br />
+								<span className={`${backgroundOn ? "bg-black" : null}`}>
+									Est. 2015
+								</span>
+							</h2>
+						</div>
+					</a>
+					{/* {props.authenticated ? <a href="/dashboard">Dashboard</a> : null} */}
+					{
+						props.authenticated ? (
+							<>
+								<a
+									className={`${backgroundOn ? "bg-black" : null}`}
+									href="/education"
+								>
+									Education
+								</a>
+								<a
+									className={`${backgroundOn ? "bg-black" : null}`}
+									href="/research"
+								>
+									Research
+								</a>
+								<a
+									className={`${backgroundOn ? "bg-black" : null}`}
+									href="/innovation"
+								>
+									Innovation
+								</a>
+								<a
+									className={`${backgroundOn ? "bg-black" : null}`}
+									href="/news"
+								>
+									News
+								</a>
+								<a
+									className={`${backgroundOn ? "bg-black" : null}`}
+									href="/Alumni"
+								>
+									Alumni
+								</a>
+								{/* <Link className={`${ backgroundOn ? "bg-black" : null}`} onClick={props.logout}>Logout</Link> */}
+							</>
+						) : null
+						// <>
+						//   <a href="/login" style={{ color: "white" }}>Login</a>
+						//   <a href="/signup">Signup</a>
+						// </>
+					}
+				</nav>
+				{/* sidebar */}
+				<div className="mobile-overlay">
+					<div className="main-nav-mobile">
+						<img id="mobile-logo" src={Logo} alt="Logo" />
+						<img
+							id="hamburger"
+							src={Hamburger}
+							alt="Hamburger bar"
+							onClick={showSidebar}
+						/>
+					</div>
+					<div className={sidebar ? "nav-menu active" : "nav-menu"}>
+						<ul className="nav-menu-items" onClick={showSidebar}>
+							<li className="navbar-toggle">
+								<img id="mobile-logo" src={Logo} alt="Logo" />
+								<Link to="#" className="menu-bars">
+									<AiIcons.AiOutlineClose />
+								</Link>
+							</li>
+							{SidebarData.map((link, index) => {
+								return (
+									<li key={index} className={link.cName}>
+										<Link to={link.path}>
+											{link.icon}
+											<span className="sidebar-title">{link.title}</span>
+										</Link>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+				</div>
+			</header>
+		</>
+	);
 };
 
 export default Navbar;
--- frontend/src/components/schoolevents/event.jsx
+++ frontend/src/components/schoolevents/event.jsx
@@ -1,98 +1,168 @@
-import React, { useState } from 'react';
-import eventData from './eventData';
-import './event.css';
+import React, { useState } from "react";
+import eventData from "./eventData";
+import "./event.css";
 
 const EventOverview = () => {
-    const [currentEvent, setCurrentEvent] = useState(0);
-    const [isLeftArrow, setIsLeftArrow] = useState(false);
-    const [isRightArrow, setIsRightArrow] = useState(false);
-    const length = eventData.length;
-    
-    const nextSlide = () => {
-        setIsLeftArrow(false);
-        setIsRightArrow(true);
-        setCurrentEvent(currentEvent === length - 1 ? 0 : currentEvent + 1);
-    }
-    const previousSlide = () => {
-        setIsRightArrow(false);
-        setIsLeftArrow(true);
-        setCurrentEvent(currentEvent === 0? length - 1 : currentEvent - 1);
-    }
-    
+	const [currentEvent, setCurrentEvent] = useState(0);
+	const [isLeftArrow, setIsLeftArrow] = useState(false);
+	const [isRightArrow, setIsRightArrow] = useState(false);
+	const length = eventData.length;
 
-    return (
-        <>
-          <section className="event-section">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6 event-item">
-                            <div>
-                                <h5 data-testid="header-spotlight">Spotlight</h5>                                   
-                                <img data-testid="image-spotlight" className="event-img img-fluid img-thumbnail" src="https://i.imgur.com/Y8GSxAg.jpg" alt="TentCity" width="600px" height="250px"/>
-                                <br/><br/>
-                                <p data-testid="paragraph1-spotlight">Christian D. - 10th Grader</p>
-                                <p data-testid="paragraph2-spotlight">Christian is a rising 10th grader who loves theater. Christian is getting ahead of the school year by learning a few geometry skills that will 
-                                    be introduced in the Fall. Christian is an honor roll student and has plans to keep it that way! </p>
-                            </div>
-                        </div>
-                        <div className="col-md-6 event-item">
-                            <div className="">
-                                <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                                    <h5 data-testid="header-online">Online Events</h5>
-                                    <div className = "arrows">
-                                        <span className = {isLeftArrow === true ? "carousel-left-arrow active" : "carousel-left-arrow"} onClick = {previousSlide}>&lt; </span>
-                                        <span className = {isRightArrow === true ? "carousel-right-arrow active" : "carousel-right-arrow"} onClick = {nextSlide}> &gt;</span>
-                                    </div>
-                                </div>
-                                {
-                                    eventData.map((e, i) => {
-                                        return(
-                                           <div className = {i === currentEvent ? "event-carousel active":"event-carousel"} key = {i}>
-                                                {i === currentEvent && (
-                                                    <>
-                                                    <img data-testid="image-online" className="event-img img-fluid img-thumbnail" src={e.img_url} alt="event1" width="600px" height="250px" />   
-                                                    <br/><br/>
-                                                    <p data-testid="paragraph1-online">{e.event_description}</p>
-                                                    <p data-testid="paragraph2-online">{e.student_description}</p>    
-                                                    </>
-                                                )}
-                                            </div>        
-                                        )
-                                    })
-                                }
+	const nextSlide = () => {
+		setIsLeftArrow(false);
+		setIsRightArrow(true);
+		setCurrentEvent(currentEvent === length - 1 ? 0 : currentEvent + 1);
+	};
+	const previousSlide = () => {
+		setIsRightArrow(false);
+		setIsLeftArrow(true);
+		setCurrentEvent(currentEvent === 0 ? length - 1 : currentEvent - 1);
+	};
 
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-6">
-                            <h5 data-testid="header-dates" className="event-title">Important Dates</h5>
-                        </div>
-                        <hr/>
-                        <div className="col-sm-1">
-                            <div>
-                                <h5 className="event-date" data-testid="header-month1">Jun. - July</h5>
-                                <h5 data-testid="header-day1">28</h5>
-                            </div>
-                        </div>
-                        <div className="col-sm-4">
-                            <h5><a href="/" className="event-links">Summer Phonics Class</a></h5>
-                        </div>
-                        <div className="col-sm-1">
-                            <div>
-                                <h5 className="event-date" data-testid="header-month2">Jun. - July</h5>
-                                <h5 data-testid="header-day2">28</h5>
-                            </div>
-                        </div>
-                     
-                        <div className="col-sm-4">
-                           <h5><a href="/" className="event-links">Yoga and Mindfulness</a></h5>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
-}
+	return (
+		<>
+			<section className="event-section">
+				<div
+					className="container"
+					data-aos="fade"
+					data-aos-offset="200"
+					data-aos-duration="1500"
+					data-aos-once="true"
+				>
+					<div className="row">
+						<div className="col-md-6 event-item">
+							<div>
+								<h5 data-testid="header-spotlight">Spotlight</h5>
+								<img
+									data-testid="image-spotlight"
+									className="event-img img-fluid img-thumbnail"
+									src="https://i.imgur.com/Y8GSxAg.jpg"
+									alt="TentCity"
+									width="600px"
+									height="250px"
+								/>
+								<br />
+								<br />
+								<p data-testid="paragraph1-spotlight">
+									Christian D. - 10th Grader
+								</p>
+								<p data-testid="paragraph2-spotlight">
+									Christian is a rising 10th grader who loves theater. Christian
+									is getting ahead of the school year by learning a few geometry
+									skills that will be introduced in the Fall. Christian is an
+									honor roll student and has plans to keep it that way!{" "}
+								</p>
+							</div>
+						</div>
+						<div className="col-md-6 event-item">
+							<div className="">
+								<div
+									style={{ display: "flex", justifyContent: "space-between" }}
+								>
+									<h5 data-testid="header-online">Online Events</h5>
+									<div className="arrows">
+										<span
+											className={
+												isLeftArrow === true
+													? "carousel-left-arrow active"
+													: "carousel-left-arrow"
+											}
+											onClick={previousSlide}
+										>
+											&lt;{" "}
+										</span>
+										<span
+											className={
+												isRightArrow === true
+													? "carousel-right-arrow active"
+													: "carousel-right-arrow"
+											}
+											onClick={nextSlide}
+										>
+											{" "}
+											&gt;
+										</span>
+									</div>
+								</div>
+								{eventData.map((e, i) => {
+									return (
+										<div
+											className={
+												i === currentEvent
+													? "event-carousel active"
+													: "event-carousel"
+											}
+											key={i}
+										>
+											{i === currentEvent && (
+												<>
+													<img
+														data-testid="image-online"
+														className="event-img img-fluid img-thumbnail"
+														src={e.img_url}
+														alt="event1"
+														width="600px"
+														height="250px"
+													/>
+													<br />
+													<br />
+													<p data-testid="paragraph1-online">
+														{e.event_description}
+													</p>
+													<p data-testid="paragraph2-online">
+														{e.student_description}
+													</p>
+												</>
+											)}
+										</div>
+									);
+								})}
+							</div>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-sm-6">
+							<h5 data-testid="header-dates" className="event-title">
+								Important Dates
+							</h5>
+						</div>
+						<hr />
+						<div className="col-sm-1">
+							<div>
+								<h5 className="event-date" data-testid="header-month1">
+									Jun. - July
+								</h5>
+								<h5 data-testid="header-day1">28</h5>
+							</div>
+						</div>
+						<div className="col-sm-4">
+							<h5>
+								<a href="/" className="event-links">
+									Summer Phonics Class
+								</a>
+							</h5>
+						</div>
+						<div className="col-sm-1">
+							<div>
+								<h5 className="event-date" data-testid="header-month2">
+									Jun. - July
+								</h5>
+								<h5 data-testid="header-day2">28</h5>
+							</div>
+						</div>
+
+						<div className="col-sm-4">
+							<h5>
+								<a href="/" className="event-links">
+									Yoga and Mindfulness
+								</a>
+							</h5>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+};
 
 export default EventOverview;
