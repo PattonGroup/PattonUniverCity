7884a2a44dfc795ba45ba290c078d84e765b9c24
Dominique Hosea <dominique11h@yahoo.com>
2023-01-20 13:32:23 -0600

    Updates

--- frontend/.storybook/pattonuTheme.js
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming';
-
-export default create({
-  base: 'light',
-  brandTitle: 'Patton UniverCity',
-  brandUrl: 'https://www.pattonunivercity.com',
-  brandImage: 'https://i.imgur.com/Lwcmxuy.png',
-});
